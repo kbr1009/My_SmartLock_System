48e9b41a355f9672c4413f057ceba9dc5dcec1fa
kbr1009 <reo109r@icloud.com>
2021-07-23 02:50:54 +0900

    lineからの情報をDynamoDBに格納する機能を実装

--- src/lambda_src/test/lambda_function2.py
+++ src/lambda_src/test/lambda_function2.py
@@ -15,39 +15,36 @@ REQUEST_HEADERS = {
         'Content-Type': 'application/json'
         }
 
-dynamoDB = boto3.resource('dynamodb')
-table = dynamoDB.Table('lock_status')
-
-def operation_put(sortKey):
-    putResponse = table.put_item(
-        Item={
-            'id': 'status',
-            'status_num': sortKey
-        }
-    )
-    if putResponse['ResponseMetadata']['HTTPStatusCode'] != 200:
-        print(putResponse)
-    else:
-       print('PUT Successed.')
-    return putResponse 
-
 def lambda_handler(event, context):
+
     logger.info(event)
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamoDB.Table('lock_status')
 
     for message_event in json.loads(event['body'])['events']:
         key_info = message_event['message']['text']
         reply_token = message_event['replyToken']
 
     if key_info == "解錠":
-        sortKey = 1
+        res = table.update_item(
+                Key={
+                    'status':1,
+                    }
+                )
         message = "解錠完了"
 
     elif key_info == "施錠":
-        sortKey = 0
+        res = table.update_item(
+                Key={
+                    'status':0,
+                    }
+                )
         message = "施錠完了"
 
     else:
         message = "Fuck you!"
+
+    print(res)
 
     params = {
             'replyToken': reply_token,
@@ -65,4 +62,4 @@ def lambda_handler(event, context):
             headers=REQUEST_HEADERS
             )
     response = urllib.request.urlopen(request, timeout=10)
-    return operation_put(sortKey)
+    return 0
