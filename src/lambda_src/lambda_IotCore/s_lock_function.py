c06a527b6dab633fb2d7097eddb8b677ad570835
kbr1009 <reo109r@icloud.com>
2021-07-27 00:34:51 +0900

    dynamodbをトリガーにawsIoTにメッセージを送る間数を実装

--- src/lambda_src/lambda_IotCore/s_lock_function.py
+++ src/lambda_src/lambda_IotCore/s_lock_function.py
@@ -1,25 +1,43 @@
 import json
 import boto3
+ 
 
+dynamoDB = boto3.resource('dynamodb')
+table = dynamoDB.Table('IotData')
 iot = boto3.client('iot-data')
 
-#関数を定義
-def lambda_handler(event, context):
 
-    #トピックを指定
-    topic = '$aws/things/ESP32/shadow/update'
-    #メッセージの内容
-    #形式通りのJsonでお願いします
-    payload = {"state": {"desired": {"led": "on"}}}
+def operation_get():
+    getResponse = table.get_item(
+        Key={
+            'id': 1
+        }
+    )
+    get_status = getResponse['Item']['status']
+    return get_status
+
+
+def lambda_handler(event, context):
+    topic = 'reo/lambda_iot'
+    latest_status = operation_get()
+
+    if  latest_status == "Open":
+        payload = {
+            "switch": "Unlock"
+            }
+    elif latest_status =="Close":
+        payload = {
+            "switch": "Lock"
+            }
+
     try:
-        #メッセージをPublish
         iot.publish(
             topic=topic,
             qos=0,
             payload=json.dumps(payload, ensure_ascii=False)
         )
-
         return "Succeeeded."
 
-    except Exception:
+    except Exception as e:
+        print(e)
         return "Failed."
